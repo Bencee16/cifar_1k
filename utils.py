8fdf9fb66a4cf3a3b3433e0990615d75fa3f3ea8
Bence Keresztury <kbence16@gmail.com>
2020-07-20 22:33:01 +0200

    change to better variable names

--- utils.py
+++ utils.py
@@ -2,32 +2,26 @@ import torchvision.models as models
 import torch.nn as nn
 
 
-
-def set_parameter_requires_grad(model, feature_extracting):
-    if feature_extracting:
+def set_parameter_requires_grad(model, freeze_weights):
+    if freeze_weights:
         for param in model.parameters():
             param.requires_grad = False
 
 
-def initialize_model(model_name, num_classes, feature_extract, use_pretrained=True):
-    # Initialize these variables which will be set in this if statement. Each of these
-    #   variables is model specific.
+def initialize_model(model_name, num_classes, use_pretrained, freeze_weights):
     model_ft = None
 
-    if model_name == "resnet":
+    if model_name == "resnet18":
         """ Resnet18
         """
         model_ft = models.resnet18(pretrained=use_pretrained)
-        set_parameter_requires_grad(model_ft, feature_extract)
+        set_parameter_requires_grad(model_ft, freeze_weights)
         num_ftrs = model_ft.fc.in_features
         model_ft.fc = nn.Sequential(
-            nn.Linear(num_ftrs, 256),
-            nn.ReLU(),
-            nn.Linear(256, 64),
+            nn.Linear(num_ftrs, 64),
             nn.ReLU(),
             nn.Linear(64, num_classes)
         )
-
 
     else:
         print("Invalid model name, exiting...")
