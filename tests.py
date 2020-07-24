1614fdecc23d89f9ce717629fe86b60f537d305e
Bence Keresztury <kbence16@gmail.com>
2020-07-24 22:39:18 +0200

    adding presentation notebook

--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ import torch
 
 
 def shape_test(model, device):
-    x = torch.randn(2,3,32,32).to(device)
+    x = torch.randn(2, 3, 32, 32).to(device)
     y = model(x)
     assert(y.size() == torch.Size([2, 10]))
     print("Output shape ok")
