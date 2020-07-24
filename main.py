838f13ee3310c6daeec2819079e22c8f0a7e230d
Bence Keresztury <kbence16@gmail.com>
2020-07-24 21:53:28 +0200

    yield ones, measure times

--- main.py
+++ main.py
@@ -6,20 +6,30 @@ from torch.utils.data import SubsetRandomSampler, SequentialSampler
 import torch.nn as nn
 import torch.optim as optim
 
-
+import datetime
 import time
 import copy
 
-from utils import initialize_model, set_params_to_update, create_coreset, save_model
-from tests import forgetting_test
+from utils import save_model
+from coreset import update_fe, create_coreset
+from model_initialization import initialize_model, set_params_to_update
+from tests import forgetting_test, shape_test
 
-path = './'
+PATH = './'
+# path to forgetting stats coming from the ResNet18 model
+forgetting_path = "preloaded_forgetting/forgetting_stats"
+
 input_size = 224
 batch_size = 64
-num_epochs = {'proxy': 10, 'core': 30}
-model_type = {'proxy': "resnet18", 'core': "resnet18"}
-use_pretrained = True
-freeze_weights = False
+num_epochs = {'proxy': 1, 'core': 1}
+model_type = {'proxy': "resnet18", 'core': "resnet50"}
+use_pretrained = {'proxy': True, 'core': True}
+freeze_weights = {'proxy': False, 'core': True}
+coreset_size = 1000
+coreset_selection_method = "reverse_forgetting_events"
+saving = True
+continue_from_selection = False
+dropout = False
 
 config = {
     'batch_size': batch_size,
@@ -27,13 +37,20 @@ config = {
     'model_type': model_type,
     'use_pretrained': use_pretrained,
     'freeze_weights': freeze_weights,
+    'core_size': coreset_size,
+    'coreset_selection_method': coreset_selection_method,
+    'continue_from_selection': continue_from_selection,
+    'dropout': dropout
 }
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 num_classes = len(classes)
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+now = str(datetime.datetime.now())
+time_string = now[:10] + '-' + now[11:13] + '-' + now[14:16]
 
-modelname = '_'.join(str(x) for x in [model_type['proxy'], num_epochs['proxy'], model_type['core'], num_epochs['core']])
+modelname = '_'.join(
+    str(x) for x in [model_type['proxy'], num_epochs['proxy'], model_type['core'], num_epochs['core'], time_string])
 
 transform_train = transforms.Compose(
     [transforms.RandomResizedCrop(input_size),
@@ -49,9 +66,6 @@ transform_eval = transforms.Compose(
 
 indices = {'train': list(range(45000)),
            'val': list(range(45000, 50000))}
-
-
-####################
 
 
 class MyDataset(torch.utils.data.Dataset):
@@ -103,10 +117,10 @@ toyloader = torch.utils.data.DataLoader(toyset, batch_size=batch_size,
                                         sampler=SequentialSampler(toyset),
                                         num_workers=2)
 
-dataloaders = {"train": trainloader, "val": valloader, "test": testloader, "toy": toyloader}
+dataloaders = {"train": toyloader, "val": valloader, "test": testloader, "toy": toyloader}
 
 
-def train_model(model, dataloaders, criterion, optimizer, num_epochs, task, device):
+def train_model(model, dataloaders, criterion, optimizer, num_epochs, task):
     since = time.time()
 
     acc_history = {'train': [], 'val': []}
@@ -144,21 +158,18 @@ def train_model(model, dataloaders, criterion, optimizer, num_epochs, task, devi
                 with torch.set_grad_enabled(phase == 'train'):
                     # Get model outputs and calculate loss
 
-                    outputs = model(inputs)
+                    outputs, ones = model(inputs)
                     loss = criterion(outputs, labels)
 
                     _, preds = torch.max(outputs, 1)
 
                     if phase == 'train':
-                        # calculate forgetting events
-                        # TODO: put this in a function
 
-                        is_correct = (preds == labels.data)
-                        correct_idxs = idxs[is_correct]
-                        nr_correct[correct_idxs] += 1
-                        is_forgotten = (is_correct < previous_status[idxs])
-                        forgetting_events[idxs[is_forgotten]] += 1
-                        previous_status[idxs] = is_correct.type(torch.float)
+                        # calculate forgetting events
+                        if task == 'proxy':
+                            previous_status, forgetting_events, nr_correct = update_fe(preds, labels, idxs,
+                                                                                       previous_status,
+                                                                                       forgetting_events, nr_correct)
 
                         # backward + optimize only if in training phase
                         loss.backward()
@@ -184,7 +195,7 @@ def train_model(model, dataloaders, criterion, optimizer, num_epochs, task, devi
 
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
-    print('Best val Acc: {:3f}'.format(best_acc))
+    print('Best val Acc: {:.3f}'.format(best_acc))
     print('\n')
 
     # load best model weights
@@ -199,48 +210,77 @@ def train_model(model, dataloaders, criterion, optimizer, num_epochs, task, devi
             inputs = inputs.to(device)
             labels = labels.to(device)
 
-            outputs = model(inputs)
+            outputs, ones = model(inputs)
             _, preds = torch.max(outputs, 1)
 
             running_corrects += torch.sum(preds == labels.data)
 
     test_acc = running_corrects.double() / len(dataloaders['test'].dataset)
     acc_history['test'] = float(test_acc * 100)
-    assert (len(dataloaders['test'].dataset) == 10000)
 
-    print(f'{task} accuracy of the network on the 10000 test images: {100 * test_acc}%')
+    assert len(dataloaders['test'].dataset) == 10000
+    print(f'{task} accuracy of the network on the 10000 test images: {round(float(100 * test_acc), 3)}% \n')
 
     forgetting_stats = {'nr_correct': nr_correct, 'forgetting_events': forgetting_events}
 
-    # todo only count forgetting stats if proxy
     return model, acc_history, forgetting_stats
 
 
-# initialize proxy model
-proxy_model = initialize_model(model_type['proxy'], num_classes, use_pretrained['proxy'], freeze_weights['proxy'], device)
-params_to_update = set_params_to_update(proxy_model, freeze_weights['proxy'])
-optimizer = optim.Adam(params_to_update, lr=0.001)
-criterion = nn.CrossEntropyLoss()
+# main script
+task = 'proxy'
+times = {}
 
-#train proxy
-proxy_model, acc_history, forgetting_stats = train_model(proxy_model, dataloaders, criterion, optimizer, num_epochs['proxy'], 'proxy')
-save_model(proxy_model, acc_history, forgetting_stats, modelname, path, config, 'proxy')
+if not continue_from_selection:
+  start_time = time.time()
+
+  # # initialize proxy model
+  proxy_model = initialize_model(model_type[task], num_classes, use_pretrained[task], freeze_weights[task], dropout, device)
+  params_to_update = set_params_to_update(proxy_model, freeze_weights[task])
+  optimizer = optim.Adam(params_to_update, lr=0.001)
+  criterion = nn.CrossEntropyLoss()
+
+  #train and save proxy
+  print('starting training the (proxy) model on the full data: \n')
+  proxy_model, acc_history, forgetting_stats = train_model(proxy_model, dataloaders, criterion, optimizer, num_epochs[task], task)
+
+  times[task] = time.time() - start_time
+
+  if saving:
+      save_model(proxy_model, acc_history, forgetting_stats, modelname, PATH, config, 'proxy', times)
+
+
+# run this section for only core-training
+if continue_from_selection:
+    forgetting_stats = torch.load(PATH + forgetting_path, map_location=device)
+    print('running coretraining using loaded forgetting stats \n')
 
 #select coreset
-forgetting_test(forgetting_stats, num_epochs['proxy'])
-coreset, coreloader = create_coreset(forgetting_stats, trainset, batch_size, indices['train'], device)
+selection_start_time = time.time()
+# forgetting_test(forgetting_stats, num_epochs['proxy'])
+print('selecting coreset \n')
+coreset, coreloader, coreset_idxs = create_coreset(forgetting_stats, trainset, batch_size, indices['train'], device, coreset_selection_method, coreset_size=coreset_size)
+print(f'{len(coreset)} sized coreset selected by method {coreset_selection_method} \n')
+times['selection'] = time.time() - selection_start_time
 
 #initialize core model
-core_model =  initialize_model(model_type['core'], num_classes, use_pretrained['core'], freeze_weights['core'], device)
-params_to_update = set_params_to_update(core_model, freeze_weights['core'])
+core_start_time = time.time()
+task = 'core'
+core_model = initialize_model(model_type[task], num_classes, use_pretrained[task], freeze_weights[task], dropout, device)
+params_to_update = set_params_to_update(core_model, freeze_weights[task])
 optimizer = optim.Adam(params_to_update, lr=0.001)
 criterion = nn.CrossEntropyLoss()
 
-#change dataloader in training phase to coreloader
+# change dataloader in training phase to coreloader
 dataloaders['train'] = coreloader
+indices['train'] = coreset_idxs
 
 # train core_model
 core_model, acc_history, _ = train_model(core_model, dataloaders, criterion, optimizer, num_epochs['core'], 'core')
 
+times[task] = time.time() - core_start_time
+if saving:
+    save_model(core_model, acc_history, _, modelname, PATH, config, 'core', times)
 
+full_time = round(sum([v for (_, v) in times.items()]))
+print(f'The end-to-end training took {full_time//3600}h, {(full_time %3600)// 60}m, {full_time % 60}s')
 
